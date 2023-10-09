fda6fdff88c55418f302865fb231cc4f8e6d60e4
Luke <ljg23a@acu.edu>
2023-10-09 18:34:51 -0500

    spaceships

---
+++ SS_ITC_110 (Galaga Clone)/Assets/Enemybulletscript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemybulletscript : MonoBehaviour
+{
+	private GameObject player;
+	private Rigidbody2D rb;
+	public float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+      rb = GetComponent<Rigidbody2D>();
+	  player = GameObject.FindGameObjectWithTag("Player");
+	  
+	  Vector3 direction = player.transform.position - transform.position;
+	  rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+	  
+	  float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+	  transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SS_ITC_110 (Galaga Clone)/Assets/Scripts/Environment/HealthPickup.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour
+{
+	public CharacterBrain characterBrain;
+	
+	
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{	
+			characterBrain.health++;
+			Destroy(this.gameObject);
+		}
+	}
+}
